04ef9d9e2b4fd23039855f04b3960efeb0d61af8
YakhlakovAlexandr <117993282+YakhlakovAlexandr@users.noreply.github.com>
2022-11-11 16:41:42 +0300

    Add files via upload

---
+++ доамшняя контрольная Яхлаков.pas
@@ -0,0 +1,18 @@
+﻿var a, b, x, y, h: real;
+begin
+  readln(a);
+  readln(b);
+  readln(h);
+  x:=a;
+  while x < b do
+  begin
+  if x < -6 then 
+    y:= ln(x)*sin(x)
+  else if x <-4 then
+    y:= e**x*ln(x)- 74
+  else
+    y:=x*(-1)*65-e**x;
+    writeln(y);
+    x:=x+h;
+    end;
+end.
